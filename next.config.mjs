0a575983f2940961865be1088d54ee6e46518702
Ahmed Khattab <125456793+ahmedkhatab17@users.noreply.github.com>
2026-06-13 19:50:33 +0300

    Change API URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/backend/:path*',
+        destination: 'http://findyourclinic.runasp.net/api/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
