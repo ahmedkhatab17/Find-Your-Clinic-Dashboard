f9bd8bbf6abe553e36710041e76013534340ef69
Ahmed Khattab <125456793+ahmedkhatab17@users.noreply.github.com>
2026-06-13 19:45:24 +0300

    Change API URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://findyourclinic.runasp.net/api',
+  baseURL: 'https://findyourclinic.runasp.net/api',
   // baseURL: 'http://localhost:5106/api', // for local  
 
   headers: {
