3ef26e3786392781277840b4872250793a30dfe3
Ahmed Khattab <125456793+ahmedkhatab17@users.noreply.github.com>
2026-06-26 18:21:20 +0300

    update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -16,7 +16,7 @@ api.interceptors.request.use((config) => {
       config.headers.Authorization = `Bearer ${token}`;
     }
   }
-  return config;
+  return config;  
 });
 
 api.interceptors.response.use(
